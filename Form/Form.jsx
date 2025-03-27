e847bb63a5bc58a0a14c47cbe69bb07677020f97
Joaquinn <joaov.b.euwest@gmail.com>
2025-03-27 14:37:17 -0300

    #sobre-componentes

---
+++ Form/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Input from './Input';
+import Button from './Button';
+
+const Form = () => {
+  return (
+    <form>
+      <label htmlFor="nome">Nome</label>
+      <Input />
+      <label htmlFor="email">Email</label>
+      <Input />
+      <p></p>
+      <Button />
+    </form>
+  );
+};
+
+export default Form;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from '/Header';
+import Footer from '/Footer';
+import Form from '/Form/Form';
 
 const App = () => {
-  return <div>React Origamid
-    <button>Adicionar</button>
-  </div>;
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+
+    </>
+  );
 };
 
 export default App;
